fcddb781f40d167a19a5b07d0e51521a826230e8
Kosuke Hata <hata.kosuke@gmail.com>
2017-10-17 14:40:36 -0700

    adding procfile

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.status(200).sendFile(__dirname + '/public/joe.jpg')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
